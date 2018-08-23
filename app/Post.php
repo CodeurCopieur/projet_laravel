91048fdc0781e9cdb72c22c3316d89926a0c4458
codwerk <louis_widdy@live.fr>
2018-08-23 11:29:12 +0200

    maj model

---
+++ app/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function caterogy() {
+        return $this->belongsTo(Category::class);
+    }
+    public function picture() {
+        return $this->hasOne(Picture::class);
+    }
+}
